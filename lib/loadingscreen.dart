07e4504d7b893f26184d04537660192877edfe70
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-28 17:10:11 +0530

    28-2-24

--- lib/loadingscreen.dart
+++ lib/loadingscreen.dart
@@ -10,6 +10,17 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff188806),
+      body: Center(
+        child: Container(
+          height: 62,
+          width: 201,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/Group 87.png'))),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grosseryapp/loadingscreen.dart';
+import 'package:grosseryapp/onbordingscreen.dart';
+import 'package:grosseryapp/signinscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +9,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoadingScreen(),
+      // home: const LoadingScreen(),
+      // home: const OnbodingScreen(),
+      home: const SigninScreen(),
     );
   }
 }
